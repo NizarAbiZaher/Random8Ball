c298a3f7aab3717281e9ad1cffe7c257fdf5cc33
NizarAbiZaher <Nizabizaher@gmail.com>
2023-09-18 18:29:50 -0400

    y

--- Magic.ts
+++ Magic.ts
@@ -5,7 +5,7 @@ const question = prompt('Enter your question for Magic 8 Ball: ');
 // random number between 1 and 8
 const randNumber = Math.floor(Math.random() * 8) + 1;
 
-// switch statement
+// switch statement to
 switch (randNumber) {
     case 1:
         console.log('It is certain');
